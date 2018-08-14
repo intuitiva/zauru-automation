04b3bfbf1f8a037408fcc464cd7303651c04e97e
Luis Pedro Sarti <sartip@intuitiva.biz>
2018-08-14 16:45:13 -0600

    enable permissions and avoid returns to avoid retries

--- get-due-clients-send-pymt-req/mail/main.go
+++ get-due-clients-send-pymt-req/mail/main.go
@@ -25,53 +25,53 @@ type ListOfUrls struct {
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 // It uses Amazon SQS request/responses provided by the aws-lambda-go/events package,
 // However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
-func Handler(sqsEvent events.SQSEvent) error {
+func Handler(sqsEvent events.SQSEvent) (string, error) {
 
-	for _, message := range sqsEvent.Records {
+	message := sqsEvent.Records[0] // just 1 record as my batch size is 1 (serverless.yml)
 
-		var listOfUrls ListOfUrls
-		json.Unmarshal([]byte(message.Body), &listOfUrls)
-		// for Zauru credentials, exclude exclusive seller, exclude payee_category
-		zauruUserEmail := listOfUrls.ZauruUserEmail
-		zauruUserToken := listOfUrls.ZauruUserToken
+	var listOfUrls ListOfUrls
+	json.Unmarshal([]byte(message.Body), &listOfUrls)
+	// for Zauru credentials, exclude exclusive seller, exclude payee_category
+	zauruUserEmail := listOfUrls.ZauruUserEmail
+	zauruUserToken := listOfUrls.ZauruUserToken
 
-		if zauruUserEmail == "" || zauruUserToken == "" {
-			log.Printf("No Zauru credentials were provided ZauruUserToken or ZauruUserEmail")
-			return nil
-		} else {
+	if zauruUserEmail == "" || zauruUserToken == "" {
+		log.Printf("No Zauru credentials were provided ZauruUserToken or ZauruUserEmail")
+		return "No Zauru credentials were provided ZauruUserToken or ZauruUserEmail", nil
+	} else {
 
-			// traveling thru all clients to GET the URLs for each one (implementing conditions with IF)
-			for _, c := range listOfUrls.Urls {
-				log.Printf(c)
-				httpClient2 := &http.Client{}
-				// Execute the HTTP get
-				reportRequest, _ := http.NewRequest(listOfUrls.Method, c, nil)
-				reportRequest.Header.Add("Content-Type", "application/json")
-				reportRequest.Header.Add("X-User-Email", zauruUserEmail)
-				reportRequest.Header.Add("X-User-Token", zauruUserToken)
-				reportResponse, reportErr := httpClient2.Do(reportRequest)
-				if reportErr != nil {
-					log.Printf(reportErr.Error() + " " + c)
-					return nil
+		// traveling thru all clients to GET the URLs for each one (implementing conditions with IF)
+		for _, c := range listOfUrls.Urls {
+			httpClient2 := &http.Client{}
+			// Execute the HTTP get
+			reportRequest, _ := http.NewRequest(listOfUrls.Method, c, nil)
+			reportRequest.Header.Add("Content-Type", "application/json")
+			reportRequest.Header.Add("X-User-Email", zauruUserEmail)
+			reportRequest.Header.Add("X-User-Token", zauruUserToken)
+			reportResponse, reportErr := httpClient2.Do(reportRequest)
+			if reportErr != nil {
+				log.Printf(reportErr.Error() + " " + c)
+				//return reportErr.Error() + " " + c, reportErr
+			} else {
+				defer reportResponse.Body.Close()
+				reportBody, reportBodyErr := ioutil.ReadAll(reportResponse.Body)
+				if reportBodyErr != nil {
+					log.Printf(reportBodyErr.Error() + " " + c)
+					//return reportBodyErr.Error() + " " + c, reportBodyErr
 				} else {
-					defer reportResponse.Body.Close()
-					reportBody, reportBodyErr := ioutil.ReadAll(reportResponse.Body)
-					if reportBodyErr != nil {
-						log.Printf(reportBodyErr.Error() + " " + c)
-						return nil
-					} else {
-						//Send an HTTPResponse back to the array of
-						var reportBodyBuffer bytes.Buffer
-						json.HTMLEscape(&reportBodyBuffer, reportBody)
-						log.Printf("%s -> %s", reportResponse.Status, strings.Join(strings.Split(reportBodyBuffer.String(), "\n"), ""), c)
-					}
+					////
+					// ON SUCCESS, (passing all validations) just print the response
+					////
+					var reportBodyBuffer bytes.Buffer
+					json.HTMLEscape(&reportBodyBuffer, reportBody)
+					log.Printf("%s -> %s", reportResponse.Status, strings.Join(strings.Split(reportBodyBuffer.String(), "\n"), ""), c)
 				}
 			}
 		}
 		log.Printf("Enviados " + strconv.Itoa(len(listOfUrls.Urls)) + " correos!!!")
 	}
 
-	return nil
+	return "Hoy si terminamos", nil
 }
 
 //func DoHTTPPost(url string, param map[string]string, ch chan<- HTTPResponse) {
