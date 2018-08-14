35a2343007cda721a1969f955eee244ae2a1922a
Lester Guerra <lester@zauru.com>
2018-08-14 00:21:51 -0600

    refactoring code

--- build-ordr-from-po-and-notify/service/main.go
+++ build-ordr-from-po-and-notify/service/main.go
@@ -1,48 +1,69 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
-	"io/ioutil"
-	"net/http"
 	"os"
+	"fmt"
+	"log"
+	"strings"
+    "github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"encoding/json"
+	"net/http"
+	"bytes"
+	"io/ioutil"
 	"strconv"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-type RequestParams struct {
-	Purchase_order_id    int
-	Payment_term_id      int
-	Seller_id            int
-	Agency_id            int
-	Recipient_email      string
-	Sender_email         string
-	Email_title          string
-	Email_entity_logo    string
-	Email_entity_name    string
-	Email_recipient_name string
-	Email_extra_cc       string
-	Email_extra_bcc      string
+type apiError struct {
+	code string
+	msg string
+	err string
 }
 
-type poData struct {
-	Reference                  string                            `json:"reference"`
-	Memo                       string                            `json:"memo"`
-	Date                       string                            `json:"date"`
-	Taxable                    bool                              `json:"taxable"`
-	Pos                        bool                              `json:"pos"`
-	Payee_id                   string                            `json:"payee_id"`
-	Order_number               string                            `json:"order_number"`
-	Payment_term_id            int                               `json:"payment_term_id"`
-	Seller_id                  int                               `json:"seller_id"`
-	Agency_id                  int                               `json:"agency_id"`
-	Invoice_details_attributes map[string]map[string]interface{} `json:"invoice_details_attributes"`
+func (e *apiError) Error() string {
+	log.Print(fmt.Sprintf(`{"code":"%s","error":"%s"}`, e.code, e.err))
+	return fmt.Sprintf(`{"code":"%s","error":"%s"}`, e.code, e.msg)
+}
+
+func (e *apiError) New(code string, msg string, err string) error {
+	if err == ""{
+		err = msg
+	}
+	return &apiError{code, msg, err}
+}
+
+var errors = &apiError{}
+
+type RequestParams struct {
+	Purchase_order_id int
+	Payment_term_id int
+	Seller_id int
+	Agency_id int
+	Recipient_email string
+	Sender_email string
+	Email_title string
+	Email_entity_logo string
+	Email_entity_name string
+	Email_recipient_name string
+	Email_extra_cc string
+	Email_extra_bcc string
+}
+
+type poData struct{
+	Reference string `json:"reference"`
+	Memo string `json:"memo"`
+	Date string `json:"date"`
+	Taxable bool `json:"taxable"`
+	Pos bool `json:"pos"`
+	Payee_id string `json:"payee_id"`
+	Order_number string `json:"order_number"`
+	Payment_term_id int `json:"payment_term_id"`
+	Seller_id int `json:"seller_id"`
+	Agency_id int `json:"agency_id"`
+	Invoice_details_attributes map[string] map[string]interface{} `json:"invoice_details_attributes"`
 }
 
 type poStruct struct {
@@ -50,173 +71,168 @@ type poStruct struct {
 }
 
 // This function make validations and return body params
-func getParams(request *events.APIGatewayProxyRequest) (*RequestParams, error) {
+func getParams(request * events.APIGatewayProxyRequest) (*RequestParams, error){
 	if request.Headers["X-User-Email-1"] == "" {
-		return nil, errors.New(`user email (1) is missing.`)
+		return nil, errors.New("401", `user email (1) is missing.`, "")
 	}
 
 	if request.Headers["X-User-Token-1"] == "" {
-		return nil, errors.New(`user token (1) is missing.`)
+		return nil, errors.New("402", `user token (1) is missing.`, "")
 	}
 
 	if request.Headers["X-User-Email-2"] == "" {
-		return nil, errors.New(`user email (2) is missing.`)
+		return nil, errors.New("403", `user email (2) is missing.`, "")
 	}
 
 	if request.Headers["X-User-Token-2"] == "" {
-		return nil, errors.New(`user token (2) is missing.`)
+		return nil, errors.New("404", `user token (2) is missing.`, "")
 	}
 
 	var params RequestParams
-
+	
 	if err := json.Unmarshal([]byte(request.Body), &params); err != nil {
-		return nil, errors.New(err.Error())
+		return nil, errors.New("501", "Internal Error", err.Error())
+	}
+	
+	if params.Purchase_order_id == 0{
+		return nil, errors.New("405", `purchase order id is missing.`, "")
 	}
 
-	if params.Purchase_order_id == 0 {
-		return nil, errors.New(`purchase order id is missing.`)
+	if params.Payment_term_id == 0{
+		return nil, errors.New("406", `payment term id is missing.`, "")
 	}
 
-	if params.Payment_term_id == 0 {
-		return nil, errors.New(`payment term id is missing.`)
+	if params.Seller_id == 0{
+		return nil, errors.New("407", `seller id is missing.`, "")
 	}
 
-	if params.Seller_id == 0 {
-		return nil, errors.New(`seller id is missing.`)
+	if params.Agency_id == 0{
+		return nil, errors.New("408", `agency id is missing.`, "")
 	}
 
-	if params.Agency_id == 0 {
-		return nil, errors.New(`agency id is missing.`)
+	if params.Recipient_email == ""{
+		return nil, errors.New("409", `recipient email is missing.`, "")
 	}
 
-	if params.Recipient_email == "" {
-		return nil, errors.New(`recipient email is missing.`)
+	if params.Email_title == ""{
+		return nil, errors.New("410", `email title is missing.`, "")
 	}
-
-	if params.Email_title == "" {
-		return nil, errors.New(`email title is missing.`)
-	}
-
-	if params.Email_recipient_name == "" {
-		return nil, errors.New(`email recipient name is missing.`)
+		
+	if params.Email_recipient_name == ""{
+		return nil, errors.New("411", `email recipient name is missing.`, "")
 	}
 
 	return &params, nil
 }
 
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Validate if api key and user email is not empty
-	params, err := getParams(&request)
+func httpRequest(url string, method string, params []byte, headers map[string]string) (interface{}, error){
+	// Configuring http request
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(params))
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"getParams. %s"}`, err.Error()), StatusCode: 500}, nil
+		return nil, errors.New("502", "Internal Error", err.Error())
 	}
-
-	// Request configuration
-	url_po := fmt.Sprintf("https://zauru.herokuapp.com/purchases/purchase_orders/%d.json", params.Purchase_order_id)
-
-	req, err := http.NewRequest("GET", url_po, bytes.NewBuffer([]byte("")))
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"http.NewRequest.po. %s"}`, err.Error()), StatusCode: 500}, nil
+	// Add header values
+	for key, value := range headers {
+		req.Header.Set(key, value)
 	}
-	req.Header.Set("X-User-Email", request.Headers["X-User-Email-1"])
-	req.Header.Set("X-User-Token", request.Headers["X-User-Token-1"])
-
-	// Sending request to get purchase order data
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"client.Do.po. %s"}`, err.Error()), StatusCode: 500}, nil
+		return nil, errors.New("503", "Internal Error", err.Error())
 	}
 	defer resp.Body.Close()
-
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"ioutil.ReadAll.po. %s"}`, err.Error()), StatusCode: 500}, nil
+		return nil, errors.New("504", "Internal Error", err.Error())
+	}
+	// Parsing json response to object
+	var data_object interface{}
+	if err := json.Unmarshal(body, &data_object); err != nil{
+		return nil, errors.New("505", "Internal Error", err.Error())
+	}
+	return data_object, nil
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Validate if api key and user email is not empty
+
+	params, err := getParams(&request)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
 	}
 
-	// Parsing json PO response to object
-	var po_object interface{}
+	// PO request setup
+	headers_po := make(map[string] string)
+	url_po := fmt.Sprintf("https://zauru.herokuapp.com/purchases/purchase_orders/%d.json", params.Purchase_order_id)
+	headers_po["X-User-Email"] = request.Headers["X-User-Email-1"]
+	headers_po["X-User-Token"] = request.Headers["X-User-Token-1"]
 
-	if err := json.Unmarshal(body, &po_object); err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"Invalid purchase order json response. %s"}`, err.Error()), StatusCode: 500}, nil
+	// Send request, getting response object
+	po_object, err := httpRequest(url_po, "GET", []byte(""), headers_po)
+	if(err != nil){
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
 	}
 
-	purchase_order := po_object.(map[string]interface{})
+	purchase_order := po_object.(map[string] interface{})
+
 
 	// Filling sale order data
 	so_data := &poData{
-		Reference:                  purchase_order["reference"].(string),
-		Memo:                       purchase_order["memo"].(string),
-		Date:                       purchase_order["issue_date"].(string),
-		Taxable:                    true,
-		Pos:                        false,
-		Payee_id:                   purchase_order["agency"].(map[string]interface{})["notes"].(string),
-		Order_number:               fmt.Sprintf("%f", purchase_order["zid"].(float64)),
-		Payment_term_id:            params.Payment_term_id,
-		Seller_id:                  params.Seller_id,
-		Agency_id:                  params.Agency_id,
-		Invoice_details_attributes: make(map[string]map[string]interface{}),
+		Reference: purchase_order["reference"].(string),
+		Memo: purchase_order["memo"].(string),
+		Date: purchase_order["issue_date"].(string),
+		Taxable: true,
+		Pos: false,
+		Payee_id: purchase_order["agency"].(map[string] interface{})["notes"].(string),
+		Order_number: fmt.Sprintf("%f", purchase_order["zid"].(float64)),
+		Payment_term_id: params.Payment_term_id,
+		Seller_id: params.Seller_id,
+		Agency_id: params.Agency_id,
+		Invoice_details_attributes: make(map[string] map[string]interface{}),
 	}
+
 	so_object := &poStruct{
 		Invoice: *so_data,
 	}
 
 	row_table := ""
-	for i, po := range purchase_order["purchase_order_details"].([]interface{}) {
-		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d", i)] = make(map[string]interface{})
-		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d", i)]["quantity"], _ = strconv.ParseFloat(po.(map[string]interface{})["booked_quantity"].(string), 32)
-		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d", i)]["item_code"] = po.(map[string]interface{})["item"].(map[string]interface{})["code"].(string)
-		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d", i)]["unit_price"] = 1.00
+	for i, po:= range purchase_order["purchase_order_details"].([] interface{}) {
+		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d",i)] = make(map[string]interface{})
+		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d",i)]["quantity"], _ = strconv.ParseFloat(po.(map[string]interface{})["booked_quantity"].(string), 32)
+		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d",i)]["item_code"] = po.(map[string]interface{})["item"].(map[string]interface{})["code"].(string)
+		so_object.Invoice.Invoice_details_attributes[fmt.Sprintf("%d",i)]["unit_price"] = 1.00
 		row_table += fmt.Sprintf(
-			`<tr>
-							<th class="tg-yw4l">%s</th>
-							<th class="tg-yw4l">%s</th>
-							<th class="tg-yw4l">%s</th>
+						`<tr>
+							<th class='tg-yw4l'>%s</th>
+							<th class='tg-yw4l'>%s</th>
+							<th class='tg-yw4l'>%s</th>
 						</tr>`,
-			po.(map[string]interface{})["item"].(map[string]interface{})["code"].(string),
-			po.(map[string]interface{})["item"].(map[string]interface{})["name"].(string),
-			po.(map[string]interface{})["booked_quantity"].(string),
+						po.(map[string]interface{})["item"].(map[string]interface{})["code"].(string),
+						po.(map[string]interface{})["item"].(map[string]interface{})["name"].(string),
+						po.(map[string]interface{})["booked_quantity"].(string),
 		)
 	}
 
 	// Parsing sale order objecto to json
 	so_json, err := json.Marshal(so_object)
-
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"json.Marshal. %s"}`, err.Error()), StatusCode: 500}, nil
+		return events.APIGatewayProxyResponse{Body: errors.New("506", "Internal Error", err.Error()).Error(), StatusCode: 500}, err
 	}
 
-	// Configuring post reques to create the new sale order
+	// SO request setup
+	headers_so := make(map[string] string)
 	url_so := "https://zauru.herokuapp.com/sales/orders.json"
-	req, err = http.NewRequest("POST", url_so, bytes.NewBuffer(so_json))
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"http.NewRequest.so. %s"}`, err.Error()), StatusCode: 500}, nil
-	}
-	req.Header.Set("X-User-Email", request.Headers["X-User-Email-2"])
-	req.Header.Set("X-User-Token", request.Headers["X-User-Token-2"])
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-type", "application/json")
-
-	client = &http.Client{}
-	resp, err = client.Do(req)
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"client.Do.so. %s"}`, err.Error()), StatusCode: 500}, nil
-	}
-	defer resp.Body.Close()
-
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"ioutil.ReadAll.so. %s"}`, err.Error()), StatusCode: 500}, nil
+	headers_so["X-User-Email"] = request.Headers["X-User-Email-2"]
+	headers_so["X-User-Token"] = request.Headers["X-User-Token-2"]
+	headers_so["Accept"] = "application/json"
+	headers_so["Content-type"] = "application/json"
+	new_so_object, err := httpRequest(url_so, "POST", so_json, headers_so)
+	if(err != nil){
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, err
 	}
 
-	// Parsing json PO response to object
-	var new_so_object interface{}
-
-	if err := json.Unmarshal(body, &new_so_object); err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"Invalid created sale order json response. %s"}`, err.Error()), StatusCode: 500}, nil
-	}
-
-	sale_order := po_object.(map[string]interface{})
+	sale_order := new_so_object.(map[string] interface{})
 
 	// Configuring SQS
 	svc := sqs.New(session.New(), &aws.Config{Region: aws.String("us-west-2")})
@@ -224,34 +240,34 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// URL to our queue
 	// TODO replace by lambda environment variable
 	qURL := os.Getenv("URL_QUEUE_AUTOMATOR_MAILER")
-
+	
 	// Building html body
 	body_html := fmt.Sprintf(`	
 					<p>Se ha generado una nueva orden de venta con el siguiente detalle:<p>
-					<style type="text/css">
+					<style type='text/css'>
 						.tg  {border-collapse:collapse;border-spacing:0;border-color:#aabcfe;margin:0px auto;}
 						.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#669;background-color:#e8edff;}
 						.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#039;background-color:#b9c9fe;}
 						.tg .tg-yw4l{vertical-align:top}
 					</style>
-					<table class="tg">
+					<table class='tg'>
 						%s
 					</table>
-					<p>Ir a la orden de venta <a href='https://zauru.herokuapp.com/sales/orders/%f'></a> <p>`,
-		row_table,
-		sale_order["id"].(float64),
-	)
+					<p>Ir a la orden de venta <a href='https://zauru.herokuapp.com/sales/orders/%.f'></a> <p>`,
+					row_table,
+					sale_order["id"].(float64),
+				)
 
 	// Building json body
 	message_body := fmt.Sprintf(
 		`{"title":"%s","body":"%s","recipient_email":"%s","entity_logo":"%s","entity_name":"%s","recipient_name":"%s","sender_name":"%s","sender_email":"%s","extra_cc":"%s","extra_bcc":"%s","attached_file_url":"","attachment_name":""}`,
 		params.Email_title,
-		body_html,
+		strings.Replace(strings.Replace(body_html,"\n", "", -1), "\t", "", -1),
 		params.Recipient_email,
 		params.Email_entity_logo,
 		params.Email_entity_name,
 		params.Email_recipient_name,
-		purchase_order["agency"].(map[string]interface{})["notes"].(string),
+		purchase_order["agency"].(map[string] interface{})["notes"].(string),
 		params.Sender_email,
 		params.Email_extra_cc,
 		params.Email_extra_bcc,
@@ -259,16 +275,16 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	// Sending SQS message
 	result, err := svc.SendMessage(&sqs.SendMessageInput{
-		DelaySeconds: aws.Int64(10),
-		MessageBody:  aws.String(message_body),
-		QueueUrl:     &qURL,
-	})
+        DelaySeconds: aws.Int64(10),
+        MessageBody: aws.String(message_body),
+        QueueUrl:    &qURL,
+    })
 
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: fmt.Sprintf(`{error:"svc.SendMessage. %s"}`, err.Error()), StatusCode: 500}, nil
-	}
-
-	return events.APIGatewayProxyResponse{Body: string(body) + *result.MessageId, StatusCode: 200}, nil
+    if err != nil {
+        return events.APIGatewayProxyResponse{Body: errors.New("507", "Internal Error", err.Error()).Error(), StatusCode: 500}, nil
+    }
+	
+	return events.APIGatewayProxyResponse{Body: *result.MessageId, StatusCode: 200}, nil
 }
 
 func main() {
